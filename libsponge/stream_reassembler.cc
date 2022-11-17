12f0b2a59ab969baeb2ed699225683925590c0ce
lzydx <2439346638@qq.com>
2022-11-17 08:41:54 -0500

    lab1 ok except win

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -18,17 +18,23 @@ StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity),
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    if(_capacity + _curindex <= index) return;
-    if(index + data.size() < _curindex) return;
+    cout << _curindex << "  " << index << "  " << data.size() << "  " << _unassamblebytes << "  " << _unorderstring.begin()->first <<endl;
+    if(eof){
+        cout << "eof is " << index + data.size() << endl;;
+    }
+    if(index + data.size() <= _curindex) return;
     int startindex = _curindex > index ? _curindex - index : 0;
     if(eof) {
         _eofindex = index + data.size();
         _isvalideof = true;
     }
-    string tobeprocess = data.substr(startindex, _capacity - _curindex);
+    string tobeprocess = data.substr(startindex);
+    // cout << _curindex << "  "<< startindex << "  "<< _capacity - _curindex << "  "<< _curindex << "  "<< _curindex << endl;
     if(index == _curindex || startindex != 0) {
-        _actualwrite += _output.write(tobeprocess);
-        _curindex += tobeprocess.size();
+        size_t writebytes = 0;
+        writebytes = _output.write(tobeprocess);
+        _actualwrite += writebytes;
+        _curindex += writebytes;
         while(!_unorderstring.empty()){
             auto item = _unorderstring.begin();
             if(item->first > _curindex) break;
@@ -38,26 +44,41 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
             // cout << nextstring<< "  " << _curindex<< "  " << item->first<< endl;
             if(_curindex - item->first < nextstring.size()){
                 nextstring = nextstring.substr(_curindex - item->first);
-                _actualwrite += _output.write(nextstring);
-                _curindex += nextstring.size();
+                writebytes = _output.write(nextstring);
+                _actualwrite += writebytes;
+                _curindex += writebytes;
             }
         }
     }
     else if(index > _curindex) {
-        // auto upper = _unorderstring.upper_bound(index);
-        // if(upper == _unorderstring.begin()){
-        //     tobeprocess = tobeprocess.substr(0, upper->first - index);
-        // }
-        // else{
-        //     auto lower = prev(upper);
-        //     int startindex = 
-        // }
-        // auto lower = prev(upper);
-
-
-
+        size_t realindex = index;
+        // find <----  if overlap
+        auto upperitem = _unorderstring.upper_bound(index);
+        if(upperitem != _unorderstring.begin()){
+            upperitem = prev(upperitem);
+            if(index < upperitem->first + upperitem->second.size()){ // left point overlao
+                realindex = upperitem->first + upperitem->second.size();
+                if(realindex >= index + tobeprocess.size()) return; // all overlapped
+                tobeprocess = tobeprocess.substr(realindex - index);
+            }
+        }
+        // find ----> if overlap
+        upperitem = _unorderstring.upper_bound(index);
+        while(upperitem != _unorderstring.end() && upperitem->first < realindex + tobeprocess.size()){
+            if(upperitem->first + upperitem->second.size() <= realindex + tobeprocess.size()){ // all overlapped
+                auto temp = upperitem;
+                upperitem = next(upperitem);
+                _unassamblebytes -= temp->second.size();
+                _unorderstring.erase(temp);
+            }
+            else{
+                _unassamblebytes -= realindex + tobeprocess.size() - upperitem->first;
+                upperitem->second = upperitem->second.substr(realindex + tobeprocess.size() - upperitem->first);
+            }
+        }
+        _unorderstring[realindex] = tobeprocess.substr(0, _capacity - _unassamblebytes);
         _unassamblebytes += tobeprocess.size();
-        _unorderstring[index] = tobeprocess;
+        _unassamblebytes = min(_capacity, _unassamblebytes);
     }
     if(_actualwrite == _eofindex && _isvalideof == true) {
         _output.end_input();
