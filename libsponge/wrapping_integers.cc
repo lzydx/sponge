efaed9c674d012c47c395c48ffa8d47310250cd9
lzydx <2439346638@qq.com>
2022-11-21 07:57:43 -0500

    lab2 int32 to 64 ok

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,8 +14,8 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    // use low 32bits of n
+    return WrappingInt32{static_cast<uint32_t> ((n)) + isn.raw_value()};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +29,10 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    int32_t delta = n - wrap(checkpoint, isn);
+
+    // recent mean delta < uint32 / 2
+    int64_t result = checkpoint + delta;
+
+    return result >= 0 ? result : result + (1ul << 32);
 }
